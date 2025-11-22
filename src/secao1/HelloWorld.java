8520f6c20c5260080d1707da6dfdf4246f165180
Sergio Oseko <osk.sergio@gmail.com>
2025-11-22 06:48:12 -0300

    chore: apenas um teste para recompilar

--- src/secao1/HelloWorld.java
+++ src/secao1/HelloWorld.java
@@ -2,6 +2,6 @@ package secao1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("Olá Mundo! 2");
     }
 }
